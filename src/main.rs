bd4a7b1dc99c38506cc8217b4c592f93cee3095c
Andrew Radcliffe <andrew.radcliffe@gmail.com>
2023-03-05 19:02:27 -0800

    Another check

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,21 @@ fn horner_eval2(x: f64, coefficients: &[f64]) -> f64 {
         .rev()
         .fold(0.0_f64, |result, &a| result.mul_add(x, a))
 }
+// fn horner_eval3(x: f64, coefficients: &[f64]) -> f64 {
+//     coefficients
+//         .iter()
+//         .rev()
+//         .reduce(|&acc, &b| acc.mul_add(x, b)).unwrap()
+// }
+fn horner_eval3(x: f64, coefficients: &[f64]) -> f64 {
+    let n = coefficients.len();
+    let a_n = coefficients[n-1];
+    coefficients[0..n-1]
+        .iter()
+        .rev()
+        .fold(a_n, |result, &a| result.mul_add(x, a))
+}
+
 
 fn main() {
     let x = 2.0;
@@ -19,4 +34,6 @@ fn main() {
     println!("{}", (c[2].mul_add(x, c[1])).mul_add(x, c[0]));
     println!("{}", horner_eval(x, &c));
     println!("{}", horner_eval2(x, &c));
+    println!("{}", horner_eval3(x, &c));
 }
+
