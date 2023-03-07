1644a6a39a1fd9859896668db1bb0bc0f0e31044
Andrew Radcliffe <andrew.radcliffe@gmail.com>
2023-03-07 07:10:25 -0800

    Experiments with `macro_rules!`

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,49 @@ fn horner_eval3(x: f64, coefficients: &[f64]) -> f64 {
         .fold(a_n, |result, &a| result.mul_add(x, a))
 }
 
+fn muladd(x: f64, a: f64, b: f64) -> f64 { x.mul_add(a, b) }
+
+macro_rules! horner {
+    ( $i:ident, $( $a:literal ),+ ) => {
+        ( ($i, $( $a, )+) )
+    };
+    ( $i:ident, ( $( $a:literal ),+ )) => {
+        ( ($i, $( $a, )+) )
+    }
+}
+
+// macro_rules! horner_1 {
+//     ( $i:ident, $( $a:literal, )* $a_n:literal, $a_n1:literal ) => {
+//         ( ( $a_n.mul_add($i, $a_n1) ), $( $a, )*)
+//     }
+// }
+
+macro_rules! horner_eval4 {
+    ( $x:tt, $a0:tt, $a1:tt ) => {
+        muladd($a1, $x, $a0)
+    };
+    ( $x:tt, $a0:tt, $( $a1:tt ),+ ) => {
+        muladd( horner_eval4!( $x, $( $a1 ),+ ), $x, $a0 )
+    };
+    ( $x:tt, $a0:tt ) => { $a0 }
+}
+
+// macro_rules! mixed_rules {
+//     () => {};
+//     (trace $name:ident; $($tail:tt)*) => {
+//         {
+//             println!(concat!(stringify!($name), " = {:?}"), $name);
+//             mixed_rules!($($tail)*);
+//         }
+//     };
+//     (trace $name:ident = $init:expr; $($tail:tt)*) => {
+//         {
+//             let $name = $init;
+//             println!(concat!(stringify!($name), " = {:?}"), $name);
+//             mixed_rules!($($tail)*);
+//         }
+//     };
+// }
 
 fn main() {
     let x = 2.0;
@@ -35,5 +78,11 @@ fn main() {
     println!("{}", horner_eval(x, &c));
     println!("{}", horner_eval2(x, &c));
     println!("{}", horner_eval3(x, &c));
+    println!("{:?}", horner!(x, 1.0, 2.0, 3.0));
+    println!("{:?}", horner!(x, (1.0, 2.0, 3.0)));
+    // println!("{:?}", horner_1!(x, 1.0, 2.0, 3.0));
+    // mixed_rules!(trace z = x + 2.0; trace y = x + 4.0;)
+    println!("{:?}", horner_eval4!(2.0, 1.0, 2.0, 3.0));
+    println!("{:?}", horner_eval4!(2.0, 1.0));
 }
 
